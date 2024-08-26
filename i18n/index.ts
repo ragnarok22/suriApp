b6fa9cbf61da6ddd5bb96ab1efc94aabc5765fda
Reinier Hernández <sasuke.reinier@gmail.com>
2024-08-26 16:27:19 -0300

    refactor(i18n): remove console.log statement

--- i18n/index.ts
+++ i18n/index.ts
@@ -15,7 +15,6 @@ const resources = {
 
 const initI18n = async () => {
   const language = Localization.getLocales()[0].languageTag;
-  console.log('language', language);
 
   i18n.use(initReactI18next).init({
     compatibilityJSON: 'v3',
