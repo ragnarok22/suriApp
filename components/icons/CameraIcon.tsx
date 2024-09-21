4223352e2ae2446c21c67127da9bde7b7d845683
Reinier Hernández <sasuke.reinier@gmail.com>
2024-09-21 14:06:21 -0300

    feat(icons): switch to Entypo icon library for CameraIcon

--- components/icons/CameraIcon.tsx
+++ components/icons/CameraIcon.tsx
@@ -1,9 +1,9 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Entypo from '@expo/vector-icons/Entypo';
 import { useThemeColor } from '@/hooks/useThemeColor';
 import { IconProps } from '.';
 
 
 export default function CameraIcon({ lightColor, darkColor, size = 28 }: IconProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
-  return <FontAwesome size={size} name='camera' style={{ color }} />
+  return <Entypo size={size} name='camera' style={{ color }} />
 }
