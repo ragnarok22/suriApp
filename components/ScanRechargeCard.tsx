5b813fcf692b163ebc4ce38d1f7af67d1262f83c
Reinier Hernández <sasuke.reinier@gmail.com>
2024-10-08 14:20:18 -0300

    feat(app): update camera plugin to expo-camera

--- components/ScanRechargeCard.tsx
+++ components/ScanRechargeCard.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
-import { useCameraDevice, useCameraPermission } from "react-native-vision-camera";
 import { StyleSheet, View } from "react-native";
-import { Camera } from "react-native-vision-camera-text-recognition";
-import { Text } from "react-native-vision-camera-text-recognition/lib/typescript/src/types";
 import { getRechargeNumber } from "@/utils/actions";
 import { ThemedText, ThemedView } from "./themed";
 
@@ -11,46 +8,9 @@ type ScanRechargeCardProps = {
 };
 
 export default function ScanRechargeCard({ onDone }: ScanRechargeCardProps) {
-  const { hasPermission, requestPermission } = useCameraPermission();
-  const device = useCameraDevice("back");
-
-  useEffect(() => {
-    requestPermission();
-  });
-
-  const handleProcessText = (data: string | Text[] | Text) => {
-    let text = Array.isArray(data) ? data.map((t) => t.resultText).join("\n") : data;
-
-    if (typeof text === 'object') {
-      text = text.resultText;
-    }
-
-    const cardNumber = getRechargeNumber(text);
-
-    if (cardNumber) {
-      onDone(cardNumber);
-    }
-  }
-
-  if (!hasPermission) {
-    return (
-      <ThemedView>
-        <ThemedText>No Camera Permission</ThemedText>
-      </ThemedView>
-    );
-  }
-
-  if (!device) {
-    return (
-      <ThemedView>
-        <ThemedText>No Camera Device</ThemedText>
-      </ThemedView>
-    );
-  }
-
   return (
     <View style={styles.cameraContainer}>
-      <Camera
+      {/*<Camera
         style={styles.camera}
         device={device}
         isActive
@@ -60,7 +20,7 @@ export default function ScanRechargeCard({ onDone }: ScanRechargeCardProps) {
         }}
         callback={handleProcessText}
         resizeMode="contain"
-      />
+      />*/}
       <View style={styles.numberPreview}>
         <View style={{ width: '80%', height: 50, borderWidth: 2, borderColor: 'rgba(0,0,0,.3)' }} />
       </View>
