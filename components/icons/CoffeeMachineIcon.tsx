095967c11d5591f75c865c1a643c37be3b8501c5
Reinier Hernández <sasuke.reinier@gmail.com>
2024-10-19 20:54:54 -0300

    feat: added some coffee icons

---
+++ components/icons/CoffeeMachineIcon.tsx
@@ -0,0 +1,12 @@
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { IconProps } from ".";
+
+export default function CoffeeMachineIcon({
+  lightColor,
+  darkColor,
+  size = 28,
+}: IconProps) {
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+  return <MaterialCommunityIcons size={size} name="coffee-maker-outline" style={{ color }} />;
+}
